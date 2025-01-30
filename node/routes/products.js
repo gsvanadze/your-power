6ddd113e089b0f3262b388cc4633648876887e57
Nika Urumashvili <urumashvili51@gmaiil.com>
2025-01-30 20:06:07 +0400

    Back-end updated

--- node/routes/products.js
+++ node/routes/products.js
@@ -10,14 +10,71 @@ const filePath = path.join(rootDir, "db-base/products.db.json");
 
 const fileReader = require('../shared/tools')
 
-productsRouter.get('/getAllProducts', (req, res) => {
-  const delay = 2000; // delay in milliseconds (2000ms = 2 seconds)
- fileReader(filePath,(err,data)=>{
-   setTimeout(()=>{
-     res.send(data)
-   },delay)
+// productsRouter.get('/getAllProducts', (req, res) => {
+//   const delay = 2000; // delay in milliseconds (2000ms = 2 seconds)
+//   fileReader(filePath, (err, data) => {
+//     setTimeout(() => {
+//       res.send(data)
+//     }, delay)
+//
+//   })
+// });
 
- })
+
+productsRouter.get('/getProductsByCategory', (req, res) => {
+
+  const delay = 2000; // delay in milliseconds (2000ms = 2 seconds)
+
+  const productType = req.query.productType;
+
+  const categoryType = req.query.categoryType;
+
+  if (productType || categoryType){
+
+    fileReader(filePath, (err, data) => {
+
+      let filteredData = data.productsList.filter((_) => {
+
+        return productType ? _.type === productType : _.categoryType === categoryType;
+
+      })
+      setTimeout(() => {
+
+        res.send(filteredData)
+
+      }, delay)
+    })
+  }else res.status(400).send('Wrong EndPoint')
+
+
+});
+
+
+productsRouter.get('/products', (req, res) => {
+
+  const delay = 2000; // delay in milliseconds (2000ms = 2 seconds)
+
+  const searchName = req.query.searchName;
+
+
+  if (searchName !== undefined){
+
+    fileReader(filePath, (err, data) => {
+
+      let filteredData = data.productsList.filter((_) => {
+
+        return _.productName.toUpperCase().startsWith(searchName.toUpperCase())
+      })
+
+      setTimeout(() => {
+
+        res.send(searchName?filteredData:data.productsList)
+
+      }, delay)
+    })
+  }else res.status(400).send('Wrong EndPoint')
+
+
 });
 
 module.exports = productsRouter;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,20 @@
-import {Component, inject, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../environments/environment';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {Component,  OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  title = 'Desktop';
-  http = inject(HttpClient)
-  fb = inject(FormBuilder)
-  private url = environment.apiUrl;
-  public registerForm = this.fb.group({
-    userName:'',
-    password:''
-  })
+export class AppComponent implements OnInit {
+
 
   ngOnInit() {
-    this.http.get(`${this.url}products/getAllProducts`).subscribe((e:any)=>{
-      console.log(e.productsList);
-    })
+
+
   }
 
-  submitForm() {
-    this.http.post(`${this.url}auth/authUser`,{userName:'SupsderAdmin@gmail.com',password:'SuperAadmin1234'}).subscribe((e:any)=>{
-      console.log(e);
-    })
-  }
+
 }
